f3936fba7cb7b795ef047707a508d8d564180f81
Matthias Probst <matth.probst@gmail.com>
2024-01-15 12:00:19 +0100

    fix doc struct

---
+++ deploy.py
@@ -0,0 +1,31 @@
+"""semi manual task
+
+1. update the ontology in versions/X.X.X/ontology.ttl
+2. run widoco
+3. run this script
+"""
+import pathlib
+
+__this_dir__ = pathlib.Path(__file__).parent
+
+import shutil
+
+
+def copy_version_to_docs():
+    version_path = __this_dir__ / 'docs/versions' / '1.0.0'
+    target_path = __this_dir__ / 'docs'
+
+    assert version_path.exists()
+    assert version_path.is_dir()
+
+    target_path.mkdir(exist_ok=True, parents=True)
+
+    shutil.copytree(version_path, target_path, dirs_exist_ok=True)
+    index_en = target_path / 'index-en.html'
+    (target_path / 'index.html').unlink(missing_ok=True)
+    index_en.rename(target_path / 'index.html')
+    assert index_en.exists() is False
+
+
+if __name__ == "__main__":
+    copy_version_to_docs()
