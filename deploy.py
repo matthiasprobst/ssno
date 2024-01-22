5ada4bd4a00b086c5de3e5883462cde775dd6781
Matthias Probst <matth.probst@gmail.com>
2024-01-22 10:52:51 +0100

    towards CI

--- deploy.py
+++ deploy.py
@@ -4,8 +4,12 @@
 2. run widoco
 3. run this script
 """
+import configparser
+import datetime
 import pathlib
 import shutil
+import subprocess
+import sys
 
 __this_dir__ = pathlib.Path(__file__).parent
 
@@ -24,12 +28,35 @@ def copy_version_to_docs():
     (target_path / 'index.html').unlink(missing_ok=True)
     index_en.rename(target_path / 'index.html')
     assert index_en.exists() is False
+    print('done copying version to docs')
 
 
 if __name__ == "__main__":
-    import sys
 
     sys.path.insert(0, '.')
+    # call batch script build.bat
+    print('Start building docs')
+
+    subprocess.run(['python', 'tests/test_graph.py'])
+
+    print(' > update modification data')
+    # open widoco config file
+    cfg_file = __this_dir__ / 'widoco.cfg'
+    config = configparser.ConfigParser()
+    with open('widoco.cfg', 'r') as f:
+        lines = [l.strip().split('=') for l in f.readlines()]
+
+    cfg_data = {l[0]: l[1] for l in lines if len(l) == 2}
+    today = datetime.datetime.today()
+    cfg_data['dateModified'] = today.strftime('%Y-%m-%d')
+
+    with open(cfg_file, 'w') as f:
+        for k, v in cfg_data.items():
+            f.write(f'\n{k}={v}')
+    script_path = __this_dir__ / 'build_onto_doc.bat'
+    print('calling ', script_path.absolute())
+    subprocess.run(str(script_path.absolute()))
+
     from generate_context import generate
 
     generate()
---
+++ tests/test_graph.py
@@ -0,0 +1,23 @@
+"""This thest is mainly taken from: https://git.rwth-aachen.de/nfdi4ing/metadata4ing/metadata4ing/-/blob/develop/tests/graph-test.py?ref_type=heads"""
+from pathlib import Path
+
+import rdflib
+from rdflib import Graph
+
+
+def parse_onto(onto_purl, onto_format):
+    g = Graph()
+    g.parse(onto_purl, format=onto_format)
+    for s, p, o in g:
+        assert isinstance(s, rdflib.URIRef), f'Error: {s} is not a URIRef'
+        assert isinstance(p, rdflib.URIRef), f'Error: {s} is not a URIRef'
+        assert isinstance(p, str), f'Error: {s} is not a str'
+    assert len(g) > 0, f'Error: No triples found in {onto_purl}.'
+    assert g, f'Error: {onto_purl} is not a graph'
+
+
+if __name__ == '__main__':
+    print('Start testing graph')
+    m4i = Path(__file__).parent.parent / 'ssno.ttl'
+    parse_onto(str(m4i), onto_format="ttl")
+    print('Finished testing graph')
