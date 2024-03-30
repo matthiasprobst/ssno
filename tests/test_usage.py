c7b07ee84fd2724ccaec48102629360d355ec677
Matthias Probst <matth.probst@gmail.com>
2024-03-30 17:41:22 +0100

    Update test_usage.py

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -25,5 +25,8 @@ class TestUserGuide(unittest.TestCase):
     def test_json_2(self):
         json2 = __this_dir__ / 'json2.json'
         snt = ssno.StandardNameTable.from_jsonld(source=str(json2))
+        self.assertEqual(snt[0].description, 'My table description')
         self.assertEqual(len(snt), 1)
-        self.assertEqual(len(snt[0].standard_name), 2)
+        self.assertEqual(len(snt[0].standard_names), 2)
+        self.assertEqual(sorted(snt[0].standard_names)[0].standard_name, 'x_velocity')
+        self.assertEqual(sorted(snt[0].standard_names)[1].standard_name, 'y_velocity')
