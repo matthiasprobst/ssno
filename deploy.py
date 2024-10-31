fdf27d72e33410bb5a5affe2f1d4c83d720df26c
MatthiasProbst <matth.probst@gmail.com>
2024-10-31 19:55:40 +0100

    fix documentaiton

--- deploy.py
+++ deploy.py
@@ -147,7 +147,8 @@ def create_version(*,
     cfg_data['abstract'] = read_lines(__this_dir__ / 'documentation' / 'Abstract.md')
     cfg_data['introduction'] = read_lines(__this_dir__ / 'documentation' / 'Introduction.md',
                                           replace_dict={"SSNO_VERSION": img_version_string.strip("v")})
-    cfg_data['description'] = read_lines(__this_dir__ / 'documentation' / 'Description.md')
+    cfg_data['description'] = read_lines(__this_dir__ / 'documentation' / 'Description.md',
+                                         replace_dict={"SSNO_VERSION": img_version_string.strip("v")})
 
     cfg_data['thisVersionURI'] = this_version_uri
     cfg_data['authors'] = 'Matthias Probst (https://orcid.org/0000-0001-8729-0482), Karlsruhe Institute ' \
