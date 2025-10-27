f5bfe9ee58d898e72d1450cbcd8ea4b83fb20de2
MatthiasProbst <matth.probst@gmail.com>
2025-10-27 17:30:27 +0100

    fix examples

--- examples/generate_cf_convention_jsonld_file.py
+++ examples/generate_cf_convention_jsonld_file.py
@@ -1,13 +1,27 @@
-from pivmetalib.ssno import StandardNameTable
-from pivmetalib.utils import download_file
+import ssnolib
+from ssnolib import StandardNameTable
+from ssnolib.dcat import Distribution
+
 
 def generate():
-    cf_contention = 'http://cfconventions.org/Data/cf-standard-names/current/src/cf-standard-name-table.xml'
-    snt_xml_filename = download_file(cf_contention, dest_filename='snt.xml')
-    xml_snt = StandardNameTable.parse(snt_xml_filename, fmt=None)
+    distribution = Distribution(
+        title='XML Table',
+        downloadURL='https://cfconventions.org/Data/cf-standard-names/current/src/cf-standard-name-table.xml',
+        mediaType='application/xml'
+    )
+    snt = ssnolib.StandardNameTable(
+        id="_:standard_name_table_v79",
+        title='CF Standard Name Table (latest version)',
+        distribution=distribution
+    )
 
     with open('cf.json', 'w') as f:
-        f.write(xml_snt.model_dump_jsonld())
+        f.write(snt.model_dump_jsonld(base_uri="https://local.org#"))
+
+    download_filename = distribution.download('cf-standard-name-table.xml')
+
+    snt_from_xml = snt.parse(download_filename, fmt='xml', make_standard_names_lowercase=True)
+    print(snt_from_xml.standard_names[:5])
 
 
 if __name__ == '__main__':
